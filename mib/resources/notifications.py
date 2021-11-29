e782fe5198f9d0216c43e93611aee23cbd2f6b94
paololanza <paolo.lanza.97@gmail.com>
2021-11-29 11:40:25 +0100

    get and create notification added

--- mib/resources/notifications.py
+++ mib/resources/notifications.py
@@ -1,4 +1,35 @@
 from flask import request, jsonify
 
-def notifications():
-    return jsonify(result='test'), 200
+from mib.dao.notification_manager import NotificationManager
+from mib.dao.models import Notification
+
+def notifications(user_id):
+
+    notifications = NotificationManager.retrieve_by_id(user_id)
+
+    response = jsonify(
+        {
+            "status" : "success",
+            "message" : "Notifications have been sent correctly",
+            "data" : notifications,
+        }
+    )
+    return response, 200
+
+def add_notifications():
+
+    data = request.get_json()
+
+    notifications = data.get("data")
+
+    for notification in notifications:
+        NotificationManager.create_notification(Notification(notification))
+
+    response = jsonify(
+        {
+            "status" : "success",
+            "message" : "Notifications added correctly"
+        }
+    )
+    
+
